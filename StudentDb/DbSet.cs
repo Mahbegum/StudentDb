d7dcdbeed57a633d0374a8e6dee66c510629d1d9
Mahbegum <mahbegum25@gmail.com>
2023-07-18 13:58:25 -0700

    Add project files.

---
+++ StudentDb/DbSet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentDBConsoleApp
+{
+    public class DbSet<T>
+    {
+        internal void Add(Program.Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
